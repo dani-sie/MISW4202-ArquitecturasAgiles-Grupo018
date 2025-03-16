38da95499b8b8a511b42c10fc4d051490bbb58df
danielsierra34 <danielsierra34@gmail.com>
2025-03-16 18:38:30 -0500

    se crea el apigateway

--- ms/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UbicacionMicroserviceController } from './app.controller';
-import { NatsClientModule } from './nats-client/nats-client.module';
-@Module({
-  imports: [NatsClientModule],
-  controllers: [UbicacionMicroserviceController],
-  providers: [],
-})
-export class AppModule {}
